be2b21215adf93d1b12743e6067e576771c33272
kdj9878 <kdj9878@gmail.com>
2023-06-22 00:13:11 +0900

    :wrench: 웹소켓 설정 파일 수정
    - ImgUtil 빈 추가

--- src/main/java/com/example/demo/config/WebsocketConfig.java
+++ src/main/java/com/example/demo/config/WebsocketConfig.java
@@ -1,7 +1,9 @@
 package com.example.demo.config;
 
 import com.example.demo.handler.WebsocketHandler;
+import com.example.demo.util.data.impl.ImgUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
@@ -12,14 +14,22 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 @EnableWebSocket
 public class WebsocketConfig implements WebSocketConfigurer {
 
+    @Value("${spring.profiles.active}")
+    private String profile;
+
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new WebsocketHandler(objectMapper()), "/ws")
+        registry.addHandler(new WebsocketHandler(objectMapper(), imgUtil()), "/ws")
                 .setAllowedOriginPatterns("*");
     }
 
     @Bean
     public ObjectMapper objectMapper(){
         return new ObjectMapper();
+    }
+
+    @Bean
+    public ImgUtil imgUtil(){
+        return new ImgUtil(profile);
     }
 }
